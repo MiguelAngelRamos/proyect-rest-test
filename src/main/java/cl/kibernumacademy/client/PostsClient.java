d33d2a53dfcae3b9f070de93de2257d765dc38a0
Miguel Ramos <return.infox@gmail.com>
2025-08-26 19:52:37 -0400

    base de proyecto

---
+++ src/main/java/cl/kibernumacademy/client/PostsClient.java
@@ -0,0 +1,19 @@
+package cl.kibernumacademy.client;
+
+public class PostsClient {
+  
+  // Listar todos los posts
+
+
+  // Obtener el detalle de un posts por su id
+
+
+
+  // Crear un Posts
+
+
+  // Actualizar un Posts
+
+
+  // Eliminar un Posts
+}

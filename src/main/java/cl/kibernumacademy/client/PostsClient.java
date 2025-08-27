d402b5ea5a428e60324d566b097c30bda3b29755
Miguel Ramos <return.infox@gmail.com>
2025-08-26 20:25:40 -0400

    post client

--- src/main/java/cl/kibernumacademy/client/PostsClient.java
+++ src/main/java/cl/kibernumacademy/client/PostsClient.java
@@ -1,20 +1,65 @@
 package cl.kibernumacademy.client;
 
+import cl.kibernumacademy.config.Specs;
+import cl.kibernumacademy.model.Post;
+
 // Clase que agrupa métodos para llamar a endpoints relacionados con "posts"
 public class PostsClient {
-  
-  // Listar todos los posts
 
+  // Listar todos los posts
+  public Response listPosts() {
+    return given()
+        .spec(Specs.request())
+        .when()
+        .get("/posts")
+        .then(Specs.ok200())
+        .extract().response();
+
+  }
 
   // Obtener el detalle de un posts por su id
 
-
+  public Response getPost(int id) {
+    return given()
+        .spec(Specs.request())
+        .when()
+        .get("/posts/{id}", id)
+        .then(Specs.ok200())
+        .extract().response();
+  }
 
   // Crear un Posts
 
+  public Response createPost(Post payload) {
+    return given()
+        .spec(Specs.request())
+        .body(payload)
+        .when()
+        .post("/posts")
+        .then(Specs.created201())
+        .extract().response();
+  }
 
   // Actualizar un Posts
 
+  public Response updatedPost(int id, Post payload) {
+    return given()
+        .spec(Specs.request())
+        .body(payload)
+        .when()
+        .put("/posts/{id}", id)
+        .then(Specs.ok200())
+        .extract().response();
+  }
 
   // Eliminar un Posts
+
+  public Response deletePost(int id) {
+    return given()
+        .spec(Specs.request())
+        .when()
+        .delete("/posts/{id}", id)
+        .then(Specs.ok200())
+        .extract().response();
+  }
 }

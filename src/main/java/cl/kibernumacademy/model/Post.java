eb0b0a98de10d40004cba5929a7a0f609d90b603
Miguel Ramos <return.infox@gmail.com>
2025-08-26 20:33:43 -0400

    codigo actualizado

--- src/main/java/cl/kibernumacademy/model/Post.java
+++ src/main/java/cl/kibernumacademy/model/Post.java
@@ -54,17 +54,7 @@ public class Post {
     return "Post [userId=" + userId + ", id=" + id + ", title=" + title + ", body=" + body + "]";
   }
 
-
 }
 
-"userId": 1,
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-}
- * 
- */
-/*
- * 
- * {
+
     
